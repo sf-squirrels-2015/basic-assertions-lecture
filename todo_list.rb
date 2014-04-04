144241ae0494bbd231074241943b393890ad5460
Ed Shadi <edshadi@gmail.com>
2014-04-04 15:00:49 -0700

    todo list class

---
+++ todo_list.rb
@@ -0,0 +1,11 @@
+class TodoList
+  attr_reader :name, :tasks
+  def initialize name, tasks=[]
+    @name = name
+    @tasks = tasks
+  end
+  def add_task task
+    @tasks << task and return true if task.is_a?(String)
+    false
+  end
+end
